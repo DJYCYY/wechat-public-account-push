d9b270a4c7991bbfc810b04c7e4c23d0f469d102
DJYCYY <112546236+DJYCYY@users.noreply.github.com>
2022-08-31 17:17:50 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,49 +1,29 @@
 export const config = {
-    /**
-     * 公众号配置
-     */
-
+    //公众号配置
     // 公众号APP_ID
     APP_ID: "wx8ce81330832cd44b",
-
     // 公众号APP_SECRET
     APP_SECRET: "fd8d2891f0024698bc6694174acff62c",
-
     // 模板消息id
     TEMPLATE_ID: "IYZk4275UI1Kmu1GuxeXJ_h_mZcGxAUl_ZU637Qn8JY",
-
     // 回调消息模板id, 用来看自己有没有发送成功的那个模板
     CALLBACK_TEMPLATE_ID: "circle299",
-
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如
     USERS: [{name: '男朋友', id: "DingJY0317"},],
     // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
     CALLBACK_USERS: [{name: '自己',id: "circle299"},],
      
-    /**
-     * 信息配置
-     */
-
+    //信息配置
     /** 天气相关 */
 
     // 所在省份
     PROVINCE: "安徽",
     // 所在城市
     CITY: "合肥",
-
-      /** 日期相关 */
-
+    
     // 在一起的日子，格式同上
     LOVE_DATE: "2022-07-30",
    
-    /** 每日一言 */
-
-    // 好文节选的内容类型
-    // 可以填写【动画，漫画，游戏，小说，原创，网络，其他】； 随机则填写 ""
-    LITERARY_PREFERENCE: ""
-
-
-    }
 
 // {{date.DATA}}  
 // 城市：{{city.DATA}}  
