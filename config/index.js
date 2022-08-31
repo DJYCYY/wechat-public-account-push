066f5bec7f31baa2c30dbbe6155d27407418f18d
DJYCYY <112546236+DJYCYY@users.noreply.github.com>
2022-08-31 17:30:12 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,29 +1,73 @@
 export const config = {
-    //公众号配置
+    /**
+     * 公众号配置
+     */
+
     // 公众号APP_ID
     APP_ID: "wx8ce81330832cd44b",
+
     // 公众号APP_SECRET
     APP_SECRET: "fd8d2891f0024698bc6694174acff62c",
+
     // 模板消息id
-    TEMPLATE_ID: "IYZk4275UI1Kmu1GuxeXJ_h_mZcGxAUl_ZU637Qn8JY",
+    TEMPLATE_ID: "	IYZk4275UI1Kmu1GuxeXJ_h_mZcGxAUl_ZU637Qn8JY",
+
     // 回调消息模板id, 用来看自己有没有发送成功的那个模板
-    CALLBACK_TEMPLATE_ID: "circle299",
+    CALLBACK_TEMPLATE_ID: "uPnmA0JCzSrS_pYFJ82oY49GyVUZPtpwMc0KlKpa8R0",
+
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如
-    USERS: [{name: '男朋友', id: "DingJY0317"},],
+    
+    USERS: [
+      {name: '宝宝', id: "DingJY0317"},
+      {name: '宝宝', id: "DingJY0317"},
+    ],
     // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
-    CALLBACK_USERS: [{name: '自己',id: "circle299"},],
+    CALLBACK_USERS: [
+      {name: '自己',id: "circle2yy"}, 
+    ],
      
-    //信息配置
+    /**
+     * 信息配置
+     */
+
     /** 天气相关 */
 
     // 所在省份
     PROVINCE: "安徽",
     // 所在城市
     CITY: "合肥",
-    
+
+    /** 重要节日相关 */
+
+    // 重要节日，修改名字为对应需要显示的名字, data 仅填月日即可, 请严格按照示例填写
+    // tpye必须填！ 只能 “生日” 和 “节日” 二选一!
+    // 生日时，name填写想要展示的名字，你可以填“美丽可爱亲亲老婆”
+    // 节日时，name填写相应展示的节日，你可以填“被搭讪纪念日”
+    FESTIVALS: [
+      {"type": "生日", "name": "猪猪猪", "year": "2000", "date": "03-17"},
+      {"type": "生日", "name": "媛媛的猪", "year": "2000", "date": "03-17"},
+    ],
+    // 限制重要节日的展示条目, 需要填写数字; 
+    // 如果为3, 则仅展示“将要到达” 的3个重要节日提醒，剩下的将被忽略
+    // 如果为0, 则默认展示全部
+    FESTIVALS_LIMIT: 3,
+
+    /** 日期相关 */
+
     // 在一起的日子，格式同上
     LOVE_DATE: "2022-07-30",
-   
+    // 结婚纪念日
+//     MARRY_DATE: "2022-07-30",
+
+
+    /** 每日一言 */
+
+    // 好文节选的内容类型
+    // 可以填写【动画，漫画，游戏，小说，原创，网络，其他】； 随机则填写 ""
+    LITERARY_PREFERENCE: ""
+
+
+    }
 
 // {{date.DATA}}  
 // 城市：{{city.DATA}}  
